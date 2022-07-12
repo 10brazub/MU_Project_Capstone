ac0217300e53081cd500dffa176064ad95f2a096
Brandon Zubia <brandonz@fb.com>
2022-07-12 11:53:23 -0700

    Fixed animation to avoid showing a black screen

--- app/src/main/java/com/example/mu_project_capstone/activities/SearchActivity.java
+++ app/src/main/java/com/example/mu_project_capstone/activities/SearchActivity.java
@@ -1,16 +1,29 @@
 package com.example.mu_project_capstone.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DownloadManager;
 import android.os.Bundle;
-import android.view.WindowManager;
+import android.text.Editable;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.SearchView;
+import android.widget.Toast;
+
 import com.example.mu_project_capstone.R;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.util.List;
 
 public class SearchActivity extends AppCompatActivity {
 
     EditText etUserSearchInput;
     EditText etUserZipcodeInput;
+    Button btnUserSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +32,20 @@ public class SearchActivity extends AppCompatActivity {
 
         etUserSearchInput = findViewById(R.id.etUserSeachInput);
         etUserZipcodeInput = findViewById(R.id.etUserZipcodeInput);
+        btnUserSearch = findViewById(R.id.btnUserSearch);
+
+
+        btnUserSearch.setOnClickListener(v -> {
+            Toast.makeText(this, "" + etUserSearchInput.getText().toString(), Toast.LENGTH_SHORT).show();
+        });
+
+
 
 
     }
-
     @Override
     public void onBackPressed() {
         super.onBackPressed();
-        overridePendingTransition(R.anim.slide_in_up, R.anim.slide_out_up);
+        overridePendingTransition(0, R.anim.slide_out_up);
     }
 }
