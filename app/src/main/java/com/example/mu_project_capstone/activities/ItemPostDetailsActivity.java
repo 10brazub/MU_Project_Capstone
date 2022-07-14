b600acdb263852668f5587492e96648cc4804eef
Brandon Zubia <brandonz@fb.com>
2022-07-13 23:37:02 -0700

    Added schedule button in ItemPostDetailsActivity

--- app/src/main/java/com/example/mu_project_capstone/activities/ItemPostDetailsActivity.java
+++ app/src/main/java/com/example/mu_project_capstone/activities/ItemPostDetailsActivity.java
@@ -2,7 +2,11 @@ package com.example.mu_project_capstone.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import com.example.mu_project_capstone.ContractorListing;
 import com.example.mu_project_capstone.R;
 
@@ -11,6 +15,7 @@ public class ItemPostDetailsActivity extends AppCompatActivity {
     TextView tvConFirstNameDetail;
     TextView tvConLastNameDetail;
     TextView tvConDescriptionDetail;
+    Button btnSchedule;
     ContractorListing contractorListing;
 
     @Override
@@ -21,6 +26,10 @@ public class ItemPostDetailsActivity extends AppCompatActivity {
         tvConFirstNameDetail = findViewById(R.id.tvConFirstNameDetail);
         tvConLastNameDetail = findViewById(R.id.tvConLastNameDetail);
         tvConDescriptionDetail = findViewById(R.id.tvConDescriptionDetail);
+        btnSchedule = findViewById(R.id.btnSchedule);
+        btnSchedule.setOnClickListener(v -> {
+            Toast.makeText(this, "CLICKED", Toast.LENGTH_SHORT).show();
+        });
 
         //handle for null
         contractorListing = getIntent().getParcelableExtra("ContractorListingDetails");
