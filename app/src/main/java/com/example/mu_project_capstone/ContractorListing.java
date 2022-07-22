5a3cc43aef7376f9e8e004cf06bd447cb6e40842
Brandon Zubia <brandonz@fb.com>
2022-07-22 10:07:51 -0700

    Added setter and getter functions for the user of ContractorListing class

--- app/src/main/java/com/example/mu_project_capstone/ContractorListing.java
+++ app/src/main/java/com/example/mu_project_capstone/ContractorListing.java
@@ -2,15 +2,26 @@ package com.example.mu_project_capstone;
 
 import com.parse.ParseClassName;
 import com.parse.ParseObject;
+import com.parse.ParseUser;
 
 @ParseClassName("ContractorListing")
 public class ContractorListing extends ParseObject {
 
+    public static final String KEY_CONTRACTOR_USER = "user";
     public static final String KEY_CONTRACTOR_FIRST_NAME = "firstName";
     public static final String KEY_CONTRACTOR_LAST_NAME = "lastName";
     public static final String KEY_CONTRACTOR_DESCRIPTION = "description";
+    public static final String KEY_CONTRACTOR_ZIPCODE = "zipcode";
 
     public ContractorListing(){}
+
+    public ParseUser getKeyContractorUser() {
+        return getParseUser(KEY_CONTRACTOR_USER);
+    }
+
+    public void setKeyContractorUser (ParseUser user){
+        put(KEY_CONTRACTOR_USER, user);
+    }
 
     public String getKeyContractorFirstName(){
         return getString(KEY_CONTRACTOR_FIRST_NAME);
@@ -35,6 +46,14 @@ public class ContractorListing extends ParseObject {
 
     public void setKeyContractorDescription(String contractorDescription) {
         put(KEY_CONTRACTOR_DESCRIPTION, contractorDescription);
+    }
+
+    public String getKeyContractorZipcode() {
+        return getString(KEY_CONTRACTOR_ZIPCODE);
+    }
+
+    public void setKeyContractorZipcode(String contractorZipcode) {
+        put(KEY_CONTRACTOR_ZIPCODE, contractorZipcode);
     }
 
 }
