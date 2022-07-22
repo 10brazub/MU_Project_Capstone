ad9e929a43d289b1e4b807056e8fd56eb1002788
Brandon Zubia <brandonz@fb.com>
2022-07-21 23:23:49 -0700

    Set up the views for the service seeker profile and service provider profile

---
+++ app/src/main/java/com/example/mu_project_capstone/fragments/ContractorProfileFragment.java
@@ -0,0 +1,84 @@
+package com.example.mu_project_capstone.fragments;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.mu_project_capstone.R;
+import com.example.mu_project_capstone.activities.LoginActivity;
+import com.parse.ParseUser;
+import static com.example.mu_project_capstone.ParseObjectKeys.*;
+
+public class ContractorProfileFragment extends Fragment {
+
+    ImageView ivContractorPhoto;
+    TextView tvProfileFirstName;
+    TextView tvProfileLastName;
+    TextView tvProfileDescription;
+    Button btnProfileLogout;
+
+    public ContractorProfileFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_contractor_profile, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        tvProfileFirstName = view.findViewById(R.id.tvProfileFirstName);
+        tvProfileLastName = view.findViewById(R.id.tvProfileLastName);
+        tvProfileDescription = view.findViewById(R.id.tvProfileDescription);
+        btnProfileLogout = view.findViewById(R.id.btnProfileLogout);
+
+        ParseUser currentUser = ParseUser.getCurrentUser();
+        String currentUserFirstName = currentUser.get(ServiceProviderFirstNameKey).toString();
+        String currentUserLastName = currentUser.get(ServiceProviderLastNameKey).toString();
+        String currentUserDescription = currentUser.get(ServiceProviderDescriptionKey).toString();
+
+        tvProfileFirstName.setText(currentUserFirstName);
+        tvProfileLastName.setText(currentUserLastName);
+        tvProfileDescription.setText(currentUserDescription);
+        btnProfileLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logoutUser();
+            }
+        });
+
+    }
+
+    private void logoutUser() {
+        ParseUser.logOut();
+        Toast.makeText(getContext(), "Logged out!", Toast.LENGTH_SHORT).show();
+        goLoginActivity();
+    }
+
+    private void goLoginActivity() {
+        Intent loginIntent = new Intent(getContext(), LoginActivity.class);
+        startActivity(loginIntent);
+        getActivity().finish();
+    }
+}
--- app/src/main/java/com/example/mu_project_capstone/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/mu_project_capstone/fragments/ProfileFragment.java
@@ -1,13 +1,31 @@
 package com.example.mu_project_capstone.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import com.example.mu_project_capstone.R;
+import com.example.mu_project_capstone.activities.LoginActivity;
+import com.parse.ParseUser;
+import static com.example.mu_project_capstone.ParseObjectKeys.*;
 
 public class ProfileFragment extends Fragment {
+
+    TextView tvUserFirstName;
+    TextView tvUserLastName;
+    TextView tvUserEmail;
+    TextView tvUserZipcode;
+    Button btnUserLogout;
+    ParseUser currentUser;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -16,5 +34,41 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        tvUserFirstName = view.findViewById(R.id.tvUserFirstName);
+        tvUserLastName = view.findViewById(R.id.tvUserLastName);
+        tvUserEmail = view.findViewById(R.id.tvUserEmail);
+        tvUserZipcode = view.findViewById(R.id.tvUserZipcode);
+        btnUserLogout = view.findViewById(R.id.btnUserLogout);
+
+        currentUser = ParseUser.getCurrentUser();
+        String currentUserFirstName = currentUser.get(ServiceSeekerFirstNameKey).toString();
+        String currentUserLastName = currentUser.get(ServiceSeekerLastNameKey).toString();
+        String currentUserEmail = currentUser.getEmail();
+        String currentUserZipcode = currentUser.get(ServiceProviderZipcodeKey).toString();
+
+        tvUserFirstName.setText(currentUserFirstName);
+        tvUserLastName.setText(currentUserLastName);
+        tvUserEmail.setText(currentUserEmail);
+        tvUserZipcode.setText(currentUserZipcode);
+        
+        btnUserLogout.setOnClickListener(v -> logoutUser());
+    }
+
+    private void logoutUser() {
+        ParseUser.logOut();
+        Toast.makeText(getContext(), "Logged out!", Toast.LENGTH_SHORT).show();
+        goLoginActivity();
+    }
+
+    private void goLoginActivity() {
+        Intent loginIntent = new Intent(getContext(), LoginActivity.class);
+        startActivity(loginIntent);
+        getActivity().finish();
     }
 }
