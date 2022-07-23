c18c108dd989a403153a959d4e3bc9cb5b10a66e
Brandon Zubia <brandonz@fb.com>
2022-07-22 17:08:10 -0700

    Began psuedocode of algorithm to rank the best service requests a contractor should take

--- app/src/main/java/com/example/mu_project_capstone/fragments/RequestsFragment.java
+++ app/src/main/java/com/example/mu_project_capstone/fragments/RequestsFragment.java
@@ -8,18 +8,19 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
-import com.example.mu_project_capstone.ContractorAvailability;
+import com.example.mu_project_capstone.models.ContractorAvailability;
 import com.example.mu_project_capstone.R;
-import com.example.mu_project_capstone.ServiceRequestsAdapter;
+import com.example.mu_project_capstone.adapters.ServiceRequestsAdapter;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import org.json.JSONArray;
 import org.json.JSONException;
-import static com.example.mu_project_capstone.ParseObjectKeys.*;
+import static com.example.mu_project_capstone.keys.ParseObjectKeys.*;
 
 public class RequestsFragment extends Fragment {
 
@@ -82,8 +83,8 @@ public class RequestsFragment extends Fragment {
         query.whereEqualTo(CONTRACTOR_AVAILABILITY_USER_KEY, ParseUser.getCurrentUser());
         query.findInBackground((contractorAvailabilityList, e) -> {
             if (e == null) {
-                ContractorAvailability currentCon = contractorAvailabilityList.get(0);
-                JSONArray listRequests = currentCon.getSundayRequests();
+                ContractorAvailability currentContractor = contractorAvailabilityList.get(0);
+                JSONArray listRequests = currentContractor.getSundayRequests();
 
                 for (int i=0; i < listRequests.length(); i++) {
                     try {
@@ -92,6 +93,13 @@ public class RequestsFragment extends Fragment {
                         ex.printStackTrace();
                     }
                 }
+
+                // I had planned to take in the list of requests and sort it based on
+                // what the service seekers are willing to pay and the amount of hours requested
+                // by the users to recommend the requests the contractor should accept based on time
+                // and pay.
+                //sortedServiceRequests(serviceRequests);
+
                 adapter.notifyDataSetChanged();
             }else {
                 Toast.makeText(context, "Could Not Retrieve Requests At This Time", Toast.LENGTH_SHORT).show();
@@ -99,4 +107,32 @@ public class RequestsFragment extends Fragment {
             }
         });
     }
+
+
+    // This function would take each user requests hourly budget and the hours they requested
+    // and be put into a container and would be sorted based on the most amount of money a contractor
+    // as efficiently as possible
+    //
+    //sortedServiceRequests(serviceRequests) {
+    //  for (int i=0; i < serviceRequests.length(); i++) {
+    //          JSONArray userRequestData = (JSONArray) serviceRequests.get(i);
+    //          start hour
+    //          end hour
+    //          int hours = userRequestData.length() - 4;
+    //          int userHourlyBudget = Integer.parseInt(userRequestData.get(1).toString());
+    //          total profit = hours * userHourlyBudget
+    //           jobScheduling(startHour, endHour, profit)
+    //    }
+    // }
+    //public int jobScheduling(startHour, endHour, profit) {
+    //        maxJsProfit = 0;
+    //        Pair arr = new Pair()
+    //        for (int i = 0; i < arr.length; i++) {
+    //          arr[i] = new Pair(startTime[i], endTime[i], profit[i]);
+    //        }
+    //        Arrays.sort(arr,(a,b)->Integer.compare(a,b));
+    //        return maxJsProfit;
+    // }
+
+
 }
